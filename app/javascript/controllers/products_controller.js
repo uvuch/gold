743774bd8ef7de012433f18f5a1a0a8bd717c439
Serge Muzyka <uvuch.gm@gmail.com>
2020-11-08 18:20:16 -0500

    Added StimulusJS library

---
+++ app/javascript/controllers/products_controller.js
@@ -0,0 +1,8 @@
+// javascript/controllers/products_controller.es6
+import { Controller } from "stimulus"
+
+export default class extends Controller {
+  connect() {
+    console.log("Hello World!");
+  }
+}
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_11_04_230709) do
+
+  create_table "products", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
