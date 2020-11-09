04ad8742f3e095ba19c2e1ddf44b7b29c040e47e
Serge Muzyka <uvuch.gm@gmail.com>
2020-11-09 01:26:00 -0500

    Implemented very basic UI. Implemented the board update/draw cycle.

--- app/javascript/controllers/dummies_controller.js
+++
@@ -1,9 +0,0 @@
-// javascript/controllers/products_controller.es6
-import { Controller } from "stimulus"
-import { GameEngine } from '../lib/game_engine';
-
-export default class extends Controller {
-  connect() {
-    var game = new GameEngine(5, 4); //5 rows, 4 columns
-  }
-}
